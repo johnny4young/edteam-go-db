aa08ec27f8e4c3e9cac752f4d53c04457113fcd2
johnny4young <asesordeprogramacion@gmail.com>
2020-08-15 11:26:24 -0500

    feat: added interface Storage

--- pkg/product/product.go
+++ pkg/product/product.go
@@ -13,3 +13,15 @@ type Model struct {
 	CreateAt     time.Time
 	UpdatedAt    time.Time
 }
+
+// Models slice of Model
+type Models []*Model
+
+// Storage interface that must implement a db storage
+type Storage interface {
+	Create(*Model) error
+	Update(*Model) error
+	GetAll() (Models, error)
+	GetByID(uint) (*Model, error)
+	Delete(uint) error
+}
